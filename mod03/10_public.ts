8d01758aabf63a478bafe10b556b67501ad0fbe9
yuhaoliao <h770320@gmail.com>
2021-01-19 16:27:08 +0800

    10 public

---
+++ mod03/10_public.ts
@@ -0,0 +1,18 @@
+namespace mod03 {
+    class Employee {
+       private Id: number;
+        name: string;
+        constructor(id: number, name: string) {
+            this.Id = id;
+            this.name = name;
+        }
+        getInfo() {
+            return `${this.Id}: ${this.name}`;
+        }
+    }
+    let emp: Employee = new Employee(1, "aaa");
+   // console.log(emp.Id,emp.name);
+   console.log(emp.name,emp.getInfo());
+   
+
+}
