0f08e99b008ed773b7426206f3d9440f12a446d5
yuhaoliao <h770320@gmail.com>
2021-01-19 11:44:10 +0800

    04 var

---
+++ mod02/04_var.ts
@@ -0,0 +1,17 @@
+let n:number=10;
+console.log(n);
+
+let m:number;
+m=20;
+console.log(m);
+
+let k=30;//也會是 number 強行別
+// k="aaa";
+console.log(k);
+
+//禁用/因為這樣會變成弱型別
+let x; //any
+x=10;
+x="aaa";
+console.log(x);
+
