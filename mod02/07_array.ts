c10303a90cb56db0374bce462b880e8894cb2bd9
yuhaoliao <h770320@gmail.com>
2021-01-19 11:51:03 +0800

    07 array

---
+++ mod02/07_array.ts
@@ -0,0 +1,9 @@
+// let seasons: string[] = ["Spring", "Summer", "Autumn", "Winter"];
+// console.log(seasons[0]);
+// seasons[0] = "春天";
+// console.log(seasons[0]);
+
+let seasons: Array<string> = ["Spring", "Summer", "Autumn", "Winter"];
+console.log(seasons[0]);
+seasons[0] = "春天";
+console.log(seasons[0]);
