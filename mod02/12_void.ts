6e06ad124d64c00bbaebf110de2d8f9800f232bf
yuhaoliao <h770320@gmail.com>
2021-01-19 13:39:11 +0800

    回傳 string & number

--- mod02/12_void.ts
+++ mod02/12_void.ts
@@ -1,5 +1,17 @@
+//不回傳
 function f1():void {
      console.log(1);
-    //return 1;
+}
+//回傳字串
+function f2():string {
+    console.log(1);
+   return "1";
+}
+//回傳數字
+function f3():number {
+    console.log(1);
+   return 1;
 }
 f1();
+f2();
+f3();
