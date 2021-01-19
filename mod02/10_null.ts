e36bbff132e48fe99d6a1db67fdd131a4e3f27ca
yuhaoliao <h770320@gmail.com>
2021-01-19 13:28:57 +0800

    加入 namespace 防止變數重覆

--- mod02/10_null.ts
+++ mod02/10_null.ts
@@ -1,13 +1,17 @@
-// let i:number=10;
-// i=null;
-// C# Nullable 的使用方法
-// union type 指定變數多個型別
-let i:number|null=10; //union type
- i=null;
+namespace mod_null {
+    // let i:number=10;
+    // i=null;
+    // C# Nullable 的使用方法
+    // union type 指定變數多個型別
+    let i:number|null=10; //union type
+    i=null;
 
-//===========================
-//  let j:number;
-//  console.log(j);
+    //===========================
+    //  let j:number;
+    //  console.log(j);
 
-let j:number|undefined;
-console.log(j);
+    let j:number|undefined;
+    console.log(j);
+    let n:number=10;
+    console.log(n);
+}
