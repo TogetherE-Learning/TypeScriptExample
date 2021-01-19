cfb043bc63a0ef19de4a443d8dd02a208f1a424e
yuhaoliao <h770320@gmail.com>
2021-01-19 14:11:56 +0800

    轉型測試

--- mod02/14_assertion.ts
+++ mod02/14_assertion.ts
@@ -3,3 +3,8 @@
 let a:any=100.34;
 console.log((<number>a).toFixed(2));
 console.log((a as number).toFixed(2));
+
+//將 a 轉成 string
+let e:string = (<number>a).toString();
+console.log(Number.parseInt(e));
+console.log(Number.parseFloat(e));
