a34fefe5ebeddccdb469acba79aa3e8ad546a764
yuhaoliao <h770320@gmail.com>
2021-01-19 13:22:53 +0800

    10 null

---
+++ mod02/10_null.ts
@@ -0,0 +1,12 @@
+// let i:number=10;
+// i=null;
+
+let i:number|null=10; //union type
+ i=null;
+
+ //===========================
+//  let j:number;
+//  console.log(j);
+
+let j:number|undefined;
+console.log(j);
