988a5d408c3c03b4da09d0a940b84e2a838a80bd
yuhaoliao <h770320@gmail.com>
2021-01-19 11:22:17 +0800

    type js vs ts

---
+++ mod02/01_type.js
@@ -0,0 +1,6 @@
+function add(x,y){
+    return x+y;
+}
+console.log(5,7);
+console.log("book","buy");
+console.log(5,"book");
---
+++ mod02/02_type.ts
@@ -0,0 +1,6 @@
+function add(x:number,y:number){
+    return x+y;
+}
+console.log(5,7);
+console.log(add("book","buy"));
+console.log(add(5,"book"));
