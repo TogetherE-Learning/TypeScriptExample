edd462fa5a0e249831f4867626b43905e9e3037e
yuhaoliao <h770320@gmail.com>
2021-01-19 15:34:43 +0800

    加入 push 功能

--- mod03/06_indexable.ts
+++ mod03/06_indexable.ts
@@ -1,9 +1,18 @@
 interface DepartmentArray {
-   readonly [index: number]: string;
-   length:number
+   [index: number]: string;
+   length:number;
+   push(...x:string[]): number;
 }
-let ar:DepartmentArray = ['IT', 'Sales', 'Accounting'];
+let arr = ['IT', 'Sales', 'Accounting'];
 
+arr.push('123');
+arr[4] = '456';
+
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
+
+let ar:DepartmentArray = ['IT', 'Sales', 'Accounting'];
 //ar[0]="demo";
 // console.log(ar[0]);
 // console.log(ar[1]);
@@ -11,5 +20,4 @@ let ar:DepartmentArray = ['IT', 'Sales', 'Accounting'];
 
 for (let i = 0; i < ar.length; i++) {
    console.log(ar[i]);
-    
 }
