32eaff8be1161901877d54098f5762db9b543adc
yuhaoliao <h770320@gmail.com>
2021-01-19 11:44:25 +0800

    06 string

---
+++ mod02/06_string.ts
@@ -0,0 +1,8 @@
+let myName : string = "mary";
+let myCpmpany : string = 'UCOM';
+let myAge : number = 20;
+let info : string = 
+`My Name is ${myName}
+My Company Name is ${myCpmpany} ,
+My age is ${myAge}`;
+console.log(info);
