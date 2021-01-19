ee9e224904419eeed09cd845882a3e81e9355c4c
yuhaoliao <h770320@gmail.com>
2021-01-19 16:14:37 +0800

    給予預設值就不用建構韓式 constructor

--- mod03/09_class.ts
+++ mod03/09_class.ts
@@ -1,13 +1,24 @@
+// class Employee {
+//     Id:number;
+//     name:string;
+//     constructor(id:number,name:string) {
+//         this.Id=id;
+//         this.name=name;
+//     }
+//     getInfo(){
+//         return `${this.Id}: ${this.name}`;
+//     }
+// }
+// let emp:Employee=new Employee(1,"aaa");
+// console.log(emp.getInfo());
+//===========================
 class Employee {
-    Id:number;
-    name:string;
-    constructor(id:number,name:string) {
-        this.Id=id;
-        this.name=name;
-    }
+    Id=1;
+    name="aaa";
+  
     getInfo(){
         return `${this.Id}: ${this.name}`;
     }
 }
-let emp:Employee=new Employee(1,"aaa");
+let emp:Employee=new Employee();
 console.log(emp.getInfo());
