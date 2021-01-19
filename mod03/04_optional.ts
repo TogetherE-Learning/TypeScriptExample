2cdd8d56c5520b07b983cb98df030609990f5d82
yuhaoliao <h770320@gmail.com>
2021-01-19 15:01:43 +0800

    調整 排版

--- mod03/04_optional.ts
+++ mod03/04_optional.ts
@@ -1,24 +1,18 @@
 namespace mod03 {
     interface MyFunc {
         x: number;
+        //y? 代表 y 變數變成選填，非必要存在
         y?: number;
     }
-    let func3 = (p: MyFunc) => {
-        // console.log(p.x, p.y);
-        // if (p.y) {
-        //     console.log(p.x, p.y)
-        // } else {
-        //     console.log(p.x)
-        // }
 
-        if (p.y===undefined) {
+    let func3 = (p: MyFunc) => {
+        if (p.y === undefined) {
             console.log(p.x)
         } else {
             console.log(p.x, p.y)
         }
-
     };
-    func3({ x: 10,y:100 });
+    func3({ x: 10, y: 100 });
     func3({ x: 200 });
-    func3({ x: 30,y:0 });
+    func3({ x: 30, y: 0 });
 }
