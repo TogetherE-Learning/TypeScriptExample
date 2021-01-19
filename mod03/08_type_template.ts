a8271cbdb2ce1ea05514e3e4da4e219c49955309
yuhaoliao <h770320@gmail.com>
2021-01-19 15:54:15 +0800

    定義 物件

--- mod03/08_type_template.ts
+++ mod03/08_type_template.ts
@@ -1,14 +1,29 @@
-namespace mod03{
+namespace mod03 {
     let employee = {
         empName: "Mary",
         age: 29,
-        showInfo:function () {
-            
-        }
+        showInfo: (function () {
+
+        })
     };
-    employee.showInfo=function () {
-        return this.empName+", "+this.age;
+    employee.showInfo = function () {
+        return this.empName + ", " + this.age;
     };
     console.log(employee.showInfo());
-    
+
+}
+namespace mod03 {
+    let employee: {
+        empName: string;
+        age: number;
+        showInfo():string;
+    };
+    employee = {
+        age : 29,
+        empName : "Tony",
+        showInfo : function () {
+            return this.empName + ", " + this.age;
+        }
+    };
+    console.log(employee.showInfo());
 }
