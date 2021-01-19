6a63953b0f78fa82af4c0c8d43ba9b24f9455ea7
yuhaoliao <h770320@gmail.com>
2021-01-19 14:52:32 +0800

    排版

--- mod03/03_interface.ts
+++ mod03/03_interface.ts
@@ -1,17 +1,17 @@
-let book={title:"ts",price:200};
-console.log(book.title,book.price);
+let book1 = { title: "ts", price: 200 };
+console.log(book1.title, book1.price);
 //--------------------------------
 // book.id=1; //error
-let book2:{title:string, price:number};
-book2={title:"js",price:300};
-console.log(book2.title,book2.price);
+let book2: { title: string, price: number };
+book2 = { title: "js", price: 300 };
+console.log(book2.title, book2.price);
 console.log("=================================");
-interface Book{
-    title:string;
-    price:number;
+interface Book {
+    title: string;
+    price: number;
 }
 
-let books:Book[]=[book,book2];
-books.forEach(element => {
-    console.log(element.title, element.price);
+let books: Array<Book> = [book1, book2];
+books.forEach(book => {
+    console.log(book.title, book.price);
 });
