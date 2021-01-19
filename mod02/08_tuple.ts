097a4ff5aaf5b66a5516800bbf194f26b5b425c7
yuhaoliao <h770320@gmail.com>
2021-01-19 11:58:54 +0800

    08 tuple

---
+++ mod02/08_tuple.ts
@@ -0,0 +1,6 @@
+var coordinate: [number, number];
+coordinate = [10,20];
+console.log(coordinate[0],coordinate[1]);
+
+var data:[number,string ,Date]=[100,"aaa",new Date()];
+console.log(data[0]+50, data[1].toUpperCase(), data[2].toLocaleTimeString("zh-tw"));
