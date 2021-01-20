6e882aa316c04c3fdb87670bf5ea97bc25e12f28
yuhaoliao <h770320@gmail.com>
2021-01-20 11:52:17 +0800

    07 調整

--- mod04/07_overloading.ts
+++ mod04/07_overloading.ts
@@ -1,10 +1,22 @@
 namespace mod04 {
     function add(x: number, y: number): number;
     function add(x: string, y: string): string;
-    function add(x: any, y: any): any {
-        return x + y;
+    function add(x: string): string;
+    function add(x: any, y?: any): any {
+        if (typeof y==="number") {
+            return x + y; 
+        }
+        if (typeof y==="string") {
+           return (<string>x).toUpperCase() +", "+y.toUpperCase();
+        }
+
+        if (y===undefined) {
+            return (x as string).toUpperCase();
+        }
+      
     }
 
     console.log(add(100,50));
     console.log(add("aaa","bbb"));
+    console.log(add("xxx"));
 }
