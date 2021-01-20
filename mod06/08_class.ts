1deccaeb22d402ee235706c3cb61fda9a662a7c2
yuhaoliao <h770320@gmail.com>
2021-01-20 16:27:23 +0800

    08 class

---
+++ mod06/08_class.ts
@@ -0,0 +1,13 @@
+namespace mod06 {
+    class GenericClass<T> {
+        add!: (x: T, y: T) => T;
+    }
+
+    let c=new GenericClass<string>();
+    c.add=(i,j)=>i.toUpperCase()+j.toUpperCase();
+    console.log(c.add("aaa","bbb"));
+
+    let c2=new GenericClass<number>();
+    c2.add=(i,j)=>i+j;
+    console.log(c2.add(50,100));
+}
