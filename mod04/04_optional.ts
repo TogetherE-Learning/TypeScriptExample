f4562deb55238abbd686ca19a16ce142cece12fd
yuhaoliao <h770320@gmail.com>
2021-01-20 11:11:02 +0800

    typeof

--- mod04/04_optional.ts
+++ mod04/04_optional.ts
@@ -5,8 +5,16 @@ namespace mod04{
         } else {
             return i+j;
         }
-       
+    }
+    function sub(i:number, j?:number):number {
+        if (typeof j === 'undefined') {
+            return i;
+        } else {
+            return i-j;
+        }
     }
     console.log(add(100,50));
     console.log(add(100));
+    console.log(sub(100,50));
+    console.log(sub(100));
 }
