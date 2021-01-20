9d80d506f8220c96e4d36b66504ce929b3d1a105
yuhaoliao <h770320@gmail.com>
2021-01-20 10:23:03 +0800

    01 function

---
+++ mod04/01_function.ts
@@ -0,0 +1,9 @@
+namespace mod04{
+    let sayHi:Function=function () {
+        console.log("Hi");
+    };
+    sayHi();
+
+    let add:Function=(i:number,j:number)=>i+j;
+    console.log(add(20,10));
+}
