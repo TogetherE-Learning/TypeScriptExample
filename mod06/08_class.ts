a281e91492dca746afa461d00dd27d0c36bccc1a
yuhaoliao <h770320@gmail.com>
2021-01-20 16:33:08 +0800

    新增註解

--- mod06/08_class.ts
+++ mod06/08_class.ts
@@ -1,13 +1,14 @@
 namespace mod06 {
+    //類似 C# 抽象類別的用法
     class GenericClass<T> {
         add!: (x: T, y: T) => T;
     }
 
-    let c=new GenericClass<string>();
-    c.add=(i,j)=>i.toUpperCase()+j.toUpperCase();
-    console.log(c.add("aaa","bbb"));
+    let c = new GenericClass<string>();
+    c.add = (i, j) => i.toUpperCase() + j.toUpperCase();
+    console.log(c.add("aaa", "bbb"));
 
-    let c2=new GenericClass<number>();
-    c2.add=(i,j)=>i+j;
-    console.log(c2.add(50,100));
+    let c2 = new GenericClass<number>();
+    c2.add = (i, j) => i + j;
+    console.log(c2.add(50, 100));
 }
