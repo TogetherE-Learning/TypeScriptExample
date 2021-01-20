e81735ec93fa61257d888e12d8a4339d30e280bc
yuhaoliao <h770320@gmail.com>
2021-01-20 15:36:45 +0800

    格式化

--- mod06/02_union.ts
+++ mod06/02_union.ts
@@ -1,9 +1,9 @@
-function getColor(c:string|{color:string}) {
-    if (typeof c==="string") {
-       console.log(c.toUpperCase());
+function getColor(c: string | { color: string }) {
+    if (typeof c === "string") {
+        console.log(c.toUpperCase());
     } else {
-     console.log(c.color);
+        console.log(c.color);
     }
 }
 getColor("red");
-getColor({color:"green"});
+getColor({ color: "green" });
