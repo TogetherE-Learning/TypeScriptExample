409b1a0d644251d6c23d9c1fabdf10ef21ab1167
yuhaoliao <h770320@gmail.com>
2021-01-20 15:34:46 +0800

    02 union

---
+++ mod06/02_union.ts
@@ -0,0 +1,9 @@
+function getColor(c:string|{color:string}) {
+    if (typeof c==="string") {
+       console.log(c.toUpperCase());
+    } else {
+     console.log(c.color);
+    }
+}
+getColor("red");
+getColor({color:"green"});
