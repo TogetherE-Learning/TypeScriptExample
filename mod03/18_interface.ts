c4d2c550a2469c41e3e69eb8ca0cfe921bad8258
yuhaoliao <h770320@gmail.com>
2021-01-20 10:10:10 +0800

    排版

--- mod03/18_interface.ts
+++ mod03/18_interface.ts
@@ -3,15 +3,16 @@ interface Shape {
     height: number;
     getArea(): number;
 }
+//跟 class 繼承不同，interface 是實作直接使用關鍵字 implements
 class Rectangle implements Shape {
-    constructor(public id:number,public width:number, public height:number) {
-        
+    constructor(public id: number, public width: number, public height: number) {
+
     }
-   
+
     getArea(): number {
-        return this.width*this.height;
+        return this.width * this.height;
     }
 }
 
-let s:Rectangle=new Rectangle(1,10,20);
+let s: Rectangle = new Rectangle(1, 10, 20);
 console.log(s.id, s.getArea());
