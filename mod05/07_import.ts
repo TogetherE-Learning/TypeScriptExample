a59d1967930e7082d5bc4a8bfbc70386d06d81fb
yuhaoliao <h770320@gmail.com>
2021-01-20 14:06:45 +0800

    import as

--- mod05/07_import.ts
+++ mod05/07_import.ts
@@ -1,4 +1,13 @@
-import { Employee,projectName } from "./06_export";
-console.log(projectName);
-let emp=new Employee(1,"aaa");
+// import { Employee,projectName } from "./06_export";
+// console.log(projectName);
+// let emp=new Employee(1,"aaa");
+// console.log(emp.getInfo());
+
+// import { Employee as MyEmployee } from "./06_export";
+// let emp=new MyEmployee(1,"aaa");
+// console.log(emp.getInfo());
+
+import * as Demo from "./06_export";
+console.log(Demo.projectName);
+let emp=new Demo.Employee(1,"aaa");
 console.log(emp.getInfo());
