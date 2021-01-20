41c166ad93325a25b7b47c2253674aaa044a11eb
yuhaoliao <h770320@gmail.com>
2021-01-20 09:31:28 +0800

    格式化

--- mod03/14_extends.ts
+++ mod03/14_extends.ts
@@ -1,24 +1,17 @@
 namespace mod03 {
     class Employee {
-
         constructor(private id: number, private name: string) {
-
         }
         getInfo() {
             return `${this.id}: ${this.name}`;
         }
     }
-
     class Sale extends Employee {
-        constructor(id:number,name:string,public bonus:number) {
-            super(id,name);
+        constructor(id: number, name: string, public bonus: number) {
+            super(id, name);
         }
     }
-
-
     let emp = new Sale(1, "aaa", 3000);
 
-    console.log( emp.getInfo(), emp.bonus);
-
-
+    console.log(emp.getInfo(), emp.bonus);
 }
