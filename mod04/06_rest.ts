9226f93e7639fcb103544d71cb53b79ef9ef7709
yuhaoliao <h770320@gmail.com>
2021-01-20 11:37:10 +0800

    多型別判斷

--- mod04/06_rest.ts
+++ mod04/06_rest.ts
@@ -1,10 +1,17 @@
-namespace mod04{
-    function sum(...numbers:number[]) {
-        let result=0;
-        numbers.forEach(i=>result+=i);
+namespace mod04 {
+    function sum(...numbers: (string|number)[]) {
+        let result = 0;
+        numbers.forEach(i => {
+            if (typeof i === "string") {
+                console.log(i);
+            }
+            else {
+                result += i;
+            }
+        });
         return result;
     }
 
-    console.log(sum(1,2,3));
-    console.log(sum(1,2,3,4,5));
+    console.log(sum(1, 2, 3));
+    console.log(sum(1, 2, "aaa", 4, 5));
 }
