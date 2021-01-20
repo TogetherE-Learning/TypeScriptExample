f507aca059af487903f13ac0774a5d296e998262
yuhaoliao <h770320@gmail.com>
2021-01-20 10:55:52 +0800

    調整

--- mod04/02_type.ts
+++ mod04/02_type.ts
@@ -1,6 +1,10 @@
 namespace mod04{
    
-    let add=(i:number,j:number)=>i+j;
-    // add=(i,j)=>i-j;
+    // let add=(i:number,j:number)=>i+j;
+    // // add=(i,j)=>i-j;
+    // console.log(add(20,10));
+    //==============================
+    let add:(i:number,j:number)=>number;
+    add=(x,y)=>x+y;
     console.log(add(20,10));
 }
