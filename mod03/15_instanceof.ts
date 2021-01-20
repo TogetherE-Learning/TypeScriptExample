02813528488f8464eee2b4be1db0e7330136694c
yuhaoliao <h770320@gmail.com>
2021-01-20 09:36:06 +0800

    15 instanceof

---
+++ mod03/15_instanceof.ts
@@ -0,0 +1,27 @@
+namespace mod03 {
+    class Employee {
+
+        constructor(private id: number, private name: string) {
+
+        }
+        getInfo() {
+            return `${this.id}: ${this.name}`;
+        }
+    }
+
+    class Sales extends Employee {
+        constructor(id:number,name:string,public bonus:number) {
+            super(id,name);
+        }
+    }
+
+
+    let emp = new Sales(1, "aaa", 3000);
+
+    console.log( emp.getInfo(), emp.bonus);
+    console.log(emp instanceof Sales);
+    console.log(emp instanceof Employee);
+    console.log(emp instanceof Object);
+    console.log(emp.toString());
+
+}
